558733f33dbc900d9b2a4c257bea9cb87e15117b
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-29 20:04:52 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,12 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print(grades)
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(students)
+students = sorted(students)
+print(students)
+new_grades = [sum(grades[0]) / len(grades[0]), sum(grades[1]) / len(grades[1]),
+              sum(grades[2]) / len(grades[2]), sum(grades[3]) / len(grades[3]),
+              sum(grades[4]) / len(grades[4])]
+print(new_grades)
+book = dict(zip(students, new_grades))
+print(book)
